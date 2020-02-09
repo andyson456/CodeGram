2b13baf91d959786bd54a21a3d9ef864e3baa216
Andrew Larson <andyson456@gmail.com>
2020-02-09 12:55:48 -0800

    Adding placeholders for followers, following, posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,20 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-3">
-
+        <div class="col-3 p-5">
+            <img src="https://yt3.ggpht.com/a/AGF-l7_sGUrG_alh-mdTIDmdcaw1GanuPL2PxVajjQ=s900-c-k-c0xffffffff-no-rj-mo" class="rounded-circle" style="width:100px; height:100px">
         </div>
-        <div class="col-9">
-            
+        <div class="col-9 p-5"]>
+            <div>
+                <h1>
+                    freeCodeGram
+                </h1>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>209</strong> posts</div>
+                <div class="pr-5"><strong>115</strong> followers</div>
+                <div class="pr-5"><strong>215</strong> following</div>
+            </div>
         </div>
     </div>
 </div>
