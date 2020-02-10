7e1040cf4b24967927923cbf4f40c26584d937a2
Andrew Larson <andyson456@gmail.com>
2020-02-09 23:38:47 -0800

    Profile pic shows up, but needs to be fixed

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="https://yt3.ggpht.com/a/AGF-l7_sGUrG_alh-mdTIDmdcaw1GanuPL2PxVajjQ=s900-c-k-c0xffffffff-no-rj-mo" class="rounded-circle" style="width:100px; height:100px">
+            <img src="/storage/{{ $user->profile->image }}" class="rounded-circle w-100 h-100">
         </div>
         <div class="col-9 p-5"]>
             <div class="d-flex justify-content-between align-items-baseline">
