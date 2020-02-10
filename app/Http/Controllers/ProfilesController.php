bc74881984169c43d4e5b0f7afcd1e1656b33349
Andrew Larson <andyson456@gmail.com>
2020-02-09 23:15:17 -0800

    Fixed postings. Fixed user update

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -29,7 +29,7 @@ class ProfilesController extends Controller
             'image' => '',
         ]);
 
-        auth()->$user->profile->update($data);
+        auth()->user()->profile->update($data);
 
         return redirect("/profile/{$user->id}");
     }
