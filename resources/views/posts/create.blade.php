2e4a8aebd2c31a2945562aa878d13feb84eb3659
Andrew Larson <andyson456@gmail.com>
2020-02-09 17:28:39 -0800

    User can now post. Validation added to post form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,6 +2,48 @@
 
 @section('content')
 <div class="container">
+    <form action="/p" enctype="multipart/form-data" method="post">
+        @csrf
 
+        <div class="row">
+            <div class="col-8 offset-2">
+
+                <div class="row">
+                    <h2>Add New Post</h2>
+                </div>
+
+                <div class="form-group row">
+                    <label for="name" class="col-md-4 col-form-label">Post Caption</label>
+
+                        <input id="caption"
+                               type="text"
+                               class="form-control @error('caption') is-invalid @enderror"
+                               name="caption"
+                                value="{{ old('caption') }}"
+                                required autocomplete="caption" autofocus>
+
+                        @error('caption')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('caption') }}</strong>
+                        </span>
+                        @enderror
+                </div>
+
+                <div class="row">
+                    <label for="image" class="col-md-4 col-form-label">Post Image</label>
+                    <input type="file" class="form-control-file" id="image" name="image">
+
+                    @error('image')
+                        <strong>{{ $errors->first('image') }}</strong>
+                    @enderror
+                </div>
+
+                <div class="row pt-4">
+                    <button class="btn btn-primary">Add New Post</button>
+                </div>
+
+            </div>
+        </div>
+    </form>
 </div>
 @endsection
