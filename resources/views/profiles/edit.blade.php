ea7171657fefc9f39be37a1ad789a799d6152225
Andrew Larson <andyson456@gmail.com>
2020-02-09 21:38:24 -0800

    Users can edit profile now. Adding policies

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -20,7 +20,7 @@
                            type="text"
                            class="form-control @error('title') is-invalid @enderror"
                            name="title"
-                           value="{{ old('title') }}"
+                           value="{{ old('title') ?? $user->profile->title}}"
                            required autocomplete="title" autofocus>
 
                     @error('title')
@@ -31,13 +31,13 @@
                 </div>
 
                 <div class="form-group row">
-                    <label for="description" class="col-md-4 col-form-label">Title</label>
+                    <label for="description" class="col-md-4 col-form-label">Description</label>
 
                     <input id="description"
                            type="text"
                            class="form-control @error('description') is-invalid @enderror"
                            name="description"
-                           value="{{ old('description') }}"
+                           value="{{ old('description') ?? $user->profile->description }}"
                            required autocomplete="description" autofocus>
 
                     @error('description')
@@ -48,13 +48,13 @@
                 </div>
 
                 <div class="form-group row">
-                    <label for="url" class="col-md-4 col-form-label">Title</label>
+                    <label for="url" class="col-md-4 col-form-label">URL</label>
 
                     <input id="url"
                            type="text"
                            class="form-control @error('url') is-invalid @enderror"
                            name="url"
-                           value="{{ old('url') }}"
+                           value="{{ old('url') ?? $user->profile->url}}"
                            required autocomplete="url" autofocus>
 
                     @error('url')
@@ -65,7 +65,7 @@
                 </div>
 
                 <div class="row">
-                    <label for="image" class="col-md-4 col-form-label">Post Image</label>
+                    <label for="image" class="col-md-4 col-form-label">Profile Image</label>
                     <input type="file" class="form-control-file" id="image" name="image">
 
                     @error('image')
@@ -74,7 +74,7 @@
                 </div>
 
                 <div class="row pt-4">
-                    <button class="btn btn-primary">Add New Post</button>
+                    <button class="btn btn-primary">Save Profile</button>
                 </div>
 
             </div>
